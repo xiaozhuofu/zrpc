75538a065f6cce633a5d39b0b857881d37441d0d
xiaozhuofu <474975656@qq.com>
2021-04-20 11:47:27 +0800

    uncommit file commit again

--- zrpc-registry/src/main/java/header.java
+++ zrpc-registry/src/main/java/header.java
@@ -1,7 +1,10 @@
+import io.netty.channel.EventLoopGroup;
+
 /**
- * @author xiaofu
+ * @author xiaofuxiaofu
  * @Description TODO
  * @createTime 2021年04月20日 11:31:00
  */
 public class header {
+    EventLoopGroup eventExecutors;
 }
